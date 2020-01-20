7c6bce6b2cd17bbf91f0f0ef7c28fbc1ad322c8c
Victoria Li <VLi@lcs.on.ca>
2020-01-20 10:56:30 -0500

    Initial Commit

---
+++ SwiftApprenticeProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftApprenticeProject
+//
+//  Created by Li, Muchen on 2020-01-20.
+//  Copyright © 2020 Li, Muchen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
